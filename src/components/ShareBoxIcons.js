03504e461f6eb63b3da06a5c51aeecf66160d59c
Mohmmad Naushad <m_naushad@Mohmmads-MacBook-Pro.local>
2023-04-05 23:56:05 +0530

    added separate js file ShareBoxIcon

---
+++ src/components/ShareBoxIcons.js
@@ -0,0 +1,12 @@
+function ShareBoxIcons() {
+  return (
+    <>
+      <div>
+        <img src="/images/user.svg" alt="" />
+        <button>Start a post</button>
+      </div>
+    </>
+  );
+}
+
+export default ShareBoxIcons;
