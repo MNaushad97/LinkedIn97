f60c3e3294e2b1aac774866ff6080d166c327fc6
Mohmmad Naushad <m_naushad@Mohmmads-MacBook-Pro.local>
2023-04-09 00:59:38 +0530

    optimised cc in now  Incenter.js looks good

--- src/components/ShareBoxIcons.js
+++ src/components/ShareBoxIcons.js
@@ -3,12 +3,10 @@ import styled from "styled-components";
 function ShareBoxIcons({ icon, iconText }) {
   return (
     <>
-      <IconTextWrapper>
-        <button>
-          <img src={`/images/${icon}.svg`} alt="" />
-          <button className="flex items-center text-sm border-none font-semibold min-h-[12px] bg-transparent outline-none text-black/60">
-            {iconText}
-          </button>
+      <IconTextWrapper className="flex justify-around mt-4 mb-2">
+        <img src={`/images/${icon}.svg`} alt="" />
+        <button className="flex items-center text-sm pl-4 border-none font-semibold min-h-[12px] bg-transparent outline-none text-black/60">
+          {iconText}
         </button>
       </IconTextWrapper>
     </>
@@ -17,20 +15,4 @@ function ShareBoxIcons({ icon, iconText }) {
 
 export default ShareBoxIcons;
 
-const CommonCard = styled.div`
-  text-align: center;
-  overflow: hidden;
-  margin-bottom: 8px;
-  background-color: #fff;
-  border-radius: 5px;
-  position: relative;
-  border: none;
-`;
-
-const IconTextWrapper = styled(CommonCard)`
-  button {
-    img {
-      margin: 0 8px 0 -2px;
-    }
-  }
-`;
+const IconTextWrapper = styled.div``;
